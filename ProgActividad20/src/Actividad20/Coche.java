c1ba995185a9dce9bdba2da532d80925a61618e8
Ayman <oumgharayman2004@gmail.com>
2024-03-13 09:38:36 +0100

    creacion Actividad20

---
+++ ProgActividad20/src/Actividad20/Coche.java
@@ -0,0 +1,12 @@
+package Actividad20;
+
+public class Coche {
+	
+	String marca;
+	String matricula;
+	String id;
+	
+	static int contadorID;
+	
+	final String 
+}
---
+++ ProgActividad20/src/Actividad20/Main.java
@@ -0,0 +1,63 @@
+package Actividad20;
+
+public class Main {
+	public static void main(String args[]) {
+		/*
+		 * Crear una clase en java que represente a una entidad Coche. Los coches
+		 * tendrán 3 atributos dinámicos.
+		 * 
+		 * id matricula marca 
+		 * 
+		 * Además, tendrá un atributo estático que llevará un
+		 * contador del atributo id. Dicho contador NO lo podrá cambiar nadie, será
+		 * gestionado por la propia clase.
+		 * 
+		 * contadorID (estático) 
+		 * 
+		 * También tendrá 2 constantes que se podrá acceder a
+		 * ellas desde cualquier lugar de la aplicación. Representarán como valor las
+		 * dos marcas más utilizadas, que serán "TOYOTA" y "RENAULT".
+		 * 
+		 * La clase tendrá solamente el constructor por defecto, que cuando se invoque,
+		 * asignará automáticamente el id al coche. Dicho id será el valor que tenga el
+		 * atributo "contadorID", y luego le incrementará el valor en 1 para que no se
+		 * dupliquen los ID del los coches.
+		 * 
+		 * Además los objetos tendrán la siguiente funcionalidad (métodos)
+		 * 
+		 * Los objetos tendrán la opción de mostrar todos sus datos por pantalla
+		 * (mediante el método toString()) La clase tendrá la opción de mostrar el valor
+		 * actual del contadorID La clase tendrá la opción de resetear el contadorID a
+		 * cero. Parte de creación de objetos
+		 * 
+		 * Habrá que crear otra clase llamada "MainCoches" que ejecute el programa
+		 * principal que muestre el siguiente menú.
+		 * 
+		 * Crear coche Mostrar coches Mostrar contadorID Resetear contadorID Salir del
+		 * programa El método estático "main" tendrá que invocar a un método DINÁMICO
+		 * llamado "arrancarPrograma()" que es donde se ejecutará toda la lógica de la
+		 * app.
+		 * 
+		 * Se creará un método para cada opción, que será llamado desde el método
+		 * "arrancarPrograma()"
+		 * 
+		 * La opción 1 pedirá la matricula y la marca (ya que el ID se asigna
+		 * automáticamente en el constructor de Coche) y lo meterá en un arrayList de
+		 * coches. Dicho arrayList deberá ser un atributo dinámico dentro de la clase
+		 * "MainCoches" y por tanto podrá ser accedido en todos los métodos de la clase.
+		 * Además, la clase "MainCoches" tendrá otro atributo para guardar el Scanner y
+		 * no tener que estar pasándolo por parámetro en todos los métodos que lo
+		 * necesiten.
+		 * 
+		 * La opción 2 recorrerá el arrayList de coches para mostrar sus datos.
+		 * 
+		 * La opción 3 mostrará el contadorID actual
+		 * 
+		 * La opción 4 reseteará el contadorID a cero
+		 * 
+		 * La opción 5 terminará el programa.
+		 */
+		
+		
+	}
+}
